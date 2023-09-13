d951cd1fc7fcca2890129415579ee8f930e0e2c3
Ali <muhammadalishakeel66@gmail.com>
2023-09-13 22:01:21 +0500

    Second Commit wit Skip and Reset Button

--- script.js
+++ script.js
@@ -1,37 +1,25 @@
+// Variables
 let container = document.querySelector(".container");
 let container2 = document.querySelector(".container2");
-
 let focusTime = document.querySelector("#focusTime");
 let focusTime2 = document.querySelector("#focusTime2");
-
-let time25 = document.querySelector("#time");
-let time5 = document.querySelector("#time2");
-
 let startBtn = document.querySelector("#startBtn");
 let startBtn2 = document.querySelector("#startBtn2");
-
+let reloadButton = document.querySelector("#reloadButton");
 let span1 = document.querySelector("#span1");
 let span2 = document.querySelector("#span2");
 let span3 = document.querySelector("#span3");
 let span4 = document.querySelector("#span4");
-
 let sound = document.querySelector("#alarmTone")
 
-// Mouse Effect
-let body = document.querySelector("body");
-let mouse = document.querySelector(".mouse");
-body.addEventListener('mousemove', (e) => {
-    mouse.style.left = e.x + "px"
-    mouse.style.top = e.y + "px"
-})
-
-// 25 Minutes Function
+// Focus Container 1 Function
 function func25() {
     container.classList.add("true")
     startBtn.addEventListener('click', function () {
-        let interval25 = setInterval(focusTimer, 1000)
         let startMinutes = 25;
         let time = startMinutes * 60;
+        let interval25 = setInterval(focusTimer, 1000)
+
         function focusTimer() {
             let minutes = Math.floor(time / 60)
             let seconds = time % 60;
@@ -45,6 +33,7 @@ function func25() {
                 span2.innerHTML = "0" + seconds
             }
             time--;
+
             if (span1.innerHTML + span2.innerHTML == 0) {
                 clearInterval(interval25)
                 container.classList.remove("true")
@@ -52,10 +41,20 @@ function func25() {
                 func5()
             }
         }
+        reloadButton.classList.add("true")
+    }, { once: true })
+
+    document.querySelector("#skipButton").addEventListener('click', () => {
+        container.classList.toggle("true")
+        func5()
+        if (container2.classList.contains("true") && container.classList.contains("true")) {
+            container2.classList.remove("true")
+            container.classList.add("true")
+        }
     })
 }
 func25()
-// 5 Minutes Function
+// Focus Container 2 Function
 function func5() {
     container2.classList.add("true")
     startBtn2.addEventListener('click', function () {
@@ -75,6 +74,7 @@ function func5() {
                 span4.innerHTML = "0" + seconds
             }
             time--;
+
             if (span3.innerHTML + span4.innerHTML == 0) {
                 clearInterval(interval5)
                 container2.classList.remove("true")
@@ -82,5 +82,12 @@ function func5() {
                 func25()
             }
         }
-    })
+        reloadButton.classList.add("true")
+    }, { once: true })
 }
+
+// Page Reload
+reloadButton.addEventListener('click', () => {
+    window.location.reload()
+    window.location.href
+})
